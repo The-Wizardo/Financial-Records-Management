b1a8dfd8fbf69b5506b965993d98635bcf2a49c8
Jawad4632 <jk8208486@gmail.com>
2026-04-01 16:30:47 +0530

    Configuration and base setup done

---
+++ src/main/java/com/FRM/FinancialRecordsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.FRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialRecordsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialRecordsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FRM/FinancialRecordsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialRecordsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
